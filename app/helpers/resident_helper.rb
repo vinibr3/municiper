6df7c803b5be2c2296f3d8399ea35a0a2ed5798e
Vinicius <vinicius.deoliveira@outlook.com>
2022-07-19 01:38:35 -0300

    Create resident address as nested form

--- app/helpers/resident_helper.rb
+++ app/helpers/resident_helper.rb
@@ -6,4 +6,8 @@ module ResidentHelper
   def format_document(value)
     "#{value[0,3]}.#{value[3,3]}.#{value[6,3]}-#{value[9,3]}"
   end
+
+  def ensure_address_for(object)
+    object.addresses.first || object.addresses.build
+  end
 end
